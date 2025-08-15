b055db77a2df18d04086ba7dd1827902a6d6afae
MHDcoderC <mohammadsajjadidev@gmail.com>
2025-08-15 18:04:54 +0330

    Fix Git merge conflicts in configuration files

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,6 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
-
-
-=======
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
-
-
->>>>>>> 90b0267bb542127759cf6f33cd61e8b7cf9e056f
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 
 export default defineConfig({
@@ -20,30 +19,3 @@ export default defineConfig({
     sourcemap: true,
   },
 })
-
-
-=======
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  base: './',
-  server: {
-    open: true,
-    host: '127.0.0.1',
-    port: 5173,
-    strictPort: true,
-    hmr: {
-      protocol: 'ws',
-      host: '127.0.0.1',
-      port: 5173,
-      clientPort: 5173,
-      path: '/vite-hmr',
-    },
-  },
-  build: {
-    sourcemap: true,
-  },
-})
-
-
->>>>>>> 90b0267bb542127759cf6f33cd61e8b7cf9e056f
